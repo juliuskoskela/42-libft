9ee33e67a4862d7e9c51ddc4c2c08584924d341d
Julius Koskela <julkoske@gmail.com>
2021-01-18 09:30:17 +0200

    Added nan, inf and -inf checks

--- src/conversions/c_ftoe.c
+++ src/conversions/c_ftoe.c
@@ -6,19 +6,33 @@
 /*   By: jkoskela <jkoskela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 08:16:06 by jkoskela          #+#    #+#             */
-/*   Updated: 2021/01/03 01:05:57 by jkoskela         ###   ########.fr       */
+/*   Updated: 2021/01/18 09:27:46 by jkoskela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/libft.h"
 
+static char		*check_naninf(double nbr)
+{
+	if (nbr != nbr)
+		return (s_dup("nan"));
+	if (nbr > 0x1.fffffffffffffp+1023)
+		return (s_dup("inf"));
+	if (nbr == log(0))
+		return (s_dup("-inf"));
+	return (NULL);
+}
+
 char			*c_ftoe(double nbr)
 {
 	char		*out;
 	char		*tmp;
+	char		*nan;
 	int			i;
 
 	i = 0;
+	if ((nan = check_naninf(nbr)))
+		return(nan);
 	while (nbr >= 10.0)
 	{
 		nbr /= 10;
